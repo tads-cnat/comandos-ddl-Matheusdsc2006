a91bc296c39b2368b0871db39d9f8ff3191e9c07
Matheusdsc2006 <matheusdosantoscosta55@gmail.com>
2024-10-16 11:46:54 -0300

    16/10

---
+++ exemplos/exemplos_ddl.sql
@@ -0,0 +1,23 @@
+/*markdown
+### CRIAR UM ESQUEMA
+*/
+
+create schema dbex;
+
+
+/*markdown
+### Criar a tabela `Cidade`
+*/
+
+create table dbex.cidade (
+    id serial not null,
+    nome varchar(150) not null,
+    estado char(2) not null,
+    primary key(id)
+);
+
+/*markdown
+### Apagar a tabela `Cidade`
+*/
+
+drop table dbex.cidade;
